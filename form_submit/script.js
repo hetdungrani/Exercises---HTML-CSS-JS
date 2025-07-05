d2e67325cdb6daec2649fce5b895d42f46a72005
Het <hetdungrani930@gmail.com>
2025-07-05 17:01:38 +0530

    2 file change

--- form_submit/script.js
+++ form_submit/script.js
@@ -1,25 +1,17 @@
 let name = document.querySelector("#name");
 let email = document.querySelector("#email");
 let number = document.querySelector("#number");
-let genderselector = document.getElementsByName("gender");
 let age = document.querySelector("#age");
 let submit = document.querySelector("#submit");
 let formselector = document.querySelector("#formselctor");
-let genderval;
 let userdata;
 
 formselector.addEventListener("submit", function (e) {
   e.preventDefault();
-  for (const gender of genderselector) {
-    if (gender.checked) {
-      genderval = gender.value;
-    }
-  }
   userdata = {
     Full_name: name.value,
     Email: email.value,
     Number: number.value,
-    Gender: genderval,
     Age: age.value,
   };
   console.log(userdata);
